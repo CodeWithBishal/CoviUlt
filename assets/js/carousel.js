88e83966cf57fb58c853dca0497de88375140f47
Bishal (git) <codewithbishal@gmail.com>
2022-07-27 04:51:35 +0530

    .

---
+++ assets/js/carousel.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $('#carousel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        speed: 1000,
+        dots:true,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                // centerMode: true,
+            }
+
+        }, {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                dots: true,
+                infinite: true,
+
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+                infinite: true,
+                autoplay: true,
+                autoplaySpeed: 2000,
+            }
+        }]
+    });
+});
---
+++ assets/js/typing.js
@@ -0,0 +1,15 @@
+var typed = new Typed('.quotes', {
+    strings: [
+        "<q>EXERCISE AND YOGA, KEEPS YOU FIT; HONEY AND GARLIC, STRENGTHEN YOUR GUT.</q>",
+        "<q>KEEP SAFE DISTANCE FOR COVID RESISTANCE; CLEAN HANDS AND WEAR FACE MASK CORONA WILL LEAVE BEFORE YOU ASK.</q>",
+        "<q>STRENGTHEN YOUR LUNGS, BY DEEP BREATHING; CORONA VIRUS WILL RUN, WITHOUT CEASING</q>",
+        "<q>NAMASTHEY, SAVES YOUR LIFE; SHAKE HAND, SHAKES YOUR LIFE</q>",
+        "<q>WEAR YOUR SAFETY GEAR; LIVE LIFE WITHOUT FEAR</q>",
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    smartBackspace: false,
+    loop: true
+});
+// initialize animation on scroll 
+AOS.init();
