0d07cf998f89a7f4d01cb2b8d9d548972dc3d9ac
Bishal (git) <codewithbishal@gmail.com>
2022-07-28 01:49:18 +0530

    .

--- assets/js/faq_api.js
+++ assets/js/faq_api.js
@@ -32,3 +32,19 @@ function faqcovidAPI() {
     };
     xhrReq.send();
 }
+
+function faqfdaAPIS(params) {
+    const xhrReq = new XMLHttpRequest();
+    xhrReq.open("get", "/assets/json/faq-fda.json", false); //synchronous request so that the below javascript file(accordion.js) can detect the elements
+
+    xhrReq.onload = () => {
+
+        var jsonData = JSON.parse(xhrReq.responseText);
+        let faqData = jsonData["faq"];
+        for (let i = 0; i < faqData.length; i++) {
+            var htmlData = '<li><div class="accordion-item"><button id="accordion-button-' + [i] + '" aria-expanded="false"><span class="accordion-title">' + faqData[i]['q'] + '</span></button><div class="accordion-content"><p>' + faqData[i]['a'] + '</p></div></div></li>';
+            list.innerHTML += htmlData;
+        }
+    };
+    xhrReq.send();
+}
