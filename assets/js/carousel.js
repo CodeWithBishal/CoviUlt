fb5af63b92b4cb765e8d722a654a84128ceecf34
Bishal (git) <codewithbishal@gmail.com>
2022-08-04 01:40:30 +0530

    update after mid eval
    added styles for impact page, added images, added more carousel and more content, init for chatbot.html, init for impact.html

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('#carousel').slick({
+    $('#carousel, #carousel-2, #carousel-3').slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
